2a5636baddc4f90e190387e497773f8d8741a921
Vlad Kosik <barafrost666@mail.ru>
2025-03-15 20:08:41 +0700

    Add the time of the rocket's disappearance

--- Assets/Game/Scripts/Game/Weapon/AbstractProjectile.cs
+++ Assets/Game/Scripts/Game/Weapon/AbstractProjectile.cs
@@ -1,10 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-namespace Game {
+namespace Game
+{
 
-    public abstract class AbstractProjectile : MonoBehaviour {
+    public abstract class AbstractProjectile : MonoBehaviour
+    {
+        [SerializeField]
+        private int _damage;
+        [SerializeField]
+        private int _speed;
+        [SerializeField]
+        private GameObject _destroyEffect;
+        [SerializeField]
+        private float _lifetime = 2f;
 
+        public int Damage => _damage;
+        public int Speed => _speed;
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.TryGetComponent<Health>(out var health))
+            {
+                health.DoDamage(new Health.Damage() { value = _damage });
+            }
+            Instantiate(_destroyEffect, gameObject.transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        public void DestructionThroughTime()
+        {
+            _lifetime -= Time.deltaTime;
+            if (_lifetime < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- Assets/Game/Scripts/Game/Weapon/RailgunProjectile.cs
+++ Assets/Game/Scripts/Game/Weapon/RailgunProjectile.cs
@@ -7,32 +7,10 @@ namespace Game
 
     public class RailgunProjectile : AbstractProjectile
     {
-
-        [SerializeField]
-        private int _damage;
-        [SerializeField]
-        private int _speed;
-        [SerializeField]
-        private GameObject _destroyEffect;
-
         private void Update()
         {
-            transform.position += transform.forward * _speed * Time.deltaTime;
+            DestructionThroughTime();
+            transform.position += transform.forward * Speed * Time.deltaTime;
         }
-        private void OnCollisionEnter(Collision collision)
-        {
-            if (collision.gameObject.TryGetComponent<Health>(out var health))
-            {
-                health.DoDamage(new Health.Damage() { value = _damage });
-            }
-            Instantiate(_destroyEffect, gameObject.transform.position, Quaternion.identity);
-            Destroy(gameObject);
-        }
-        //урон
-        //Скорость 
-
-        //Летит вперед и при столкновении наносит урон врагу
-        //Через GetComponent<Health> проверям надо ли наносить урон DoDamage(new Health.Damage(){value = _damage})
-        //Снаряд должен уничтожаться при любом столкновении 
     }
 }
